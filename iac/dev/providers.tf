978da41b0e30b424ab5602d47b23116fcd965540
Toan Trinh <120695318+comicalwriter@users.noreply.github.com>
2024-02-16 14:39:08 +0700

    Update providers.tf

--- iac/dev/providers.tf
+++ iac/dev/providers.tf
@@ -4,13 +4,10 @@ terraform {
       source  = "hashicorp/aws"
     }
   }
-
-  # Set version constraints for providers here
-  required_version = ">= 0.12"  # Example version constraint for Terraform itself
 }
 
 provider "aws" {
   region = "us-east-1"  # Set your desired default region here
 }
 
-# Define your AWS resources below...
+
