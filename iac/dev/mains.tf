07a40326a331abdac2129ebee159f7f6ec898e5f
Toan Trinh <120695318+comicalwriter@users.noreply.github.com>
2024-02-19 11:32:25 +0700

    Update mains.tf

--- iac/dev/mains.tf
+++ iac/dev/mains.tf
@@ -1,5 +1,34 @@
 resource "aws_s3_bucket" "example" {
-
   bucket = "my-unique-bucket-name"
   acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = "log-bucket"
+    target_prefix = "s3-logs/"
+  }
+
+  lifecycle_rule {
+    enabled = true
+    prefix  = ""
+    tags = {
+      Expiration = "30"
+    }
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
