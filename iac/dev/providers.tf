db3ccec5a1a44f5f40ac859e0b4feb5b0696e7db
Toan Trinh <120695318+comicalwriter@users.noreply.github.com>
2024-02-19 11:33:34 +0700

    Update providers.tf

--- iac/dev/providers.tf
+++ iac/dev/providers.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.37"
+      version = ">= 3.37, < 4.0"  # Specify the version constraint as a string
     }
   }
 }
+
 
 provider "aws" {
   region = "us-east-1"  # Set your desired default region here
